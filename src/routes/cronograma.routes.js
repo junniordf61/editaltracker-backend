9890ea24e61f023cea3d6a42cd797df350d570d5
junniordf61 <junniordf61@gmail.com>
2026-01-20 12:32:01 -0300

    fix cronograma routes router definition

--- src/routes/cronograma.routes.js
+++ src/routes/cronograma.routes.js
@@ -1,1 +1,9 @@
+import { Router } from 'express';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { criarCronograma } from '../controllers/cronogramaController.js';
+
+const router = Router();
+
 router.post('/gerar', authMiddleware, criarCronograma);
+
+export default router;
